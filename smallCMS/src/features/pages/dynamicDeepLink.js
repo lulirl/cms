afd5fac4dbe7bb5ecdcf87ee67f385869f7c44ee
Ignacio Padilla <ignaciop@nicasource.com>
2023-08-27 14:25:16 -0300

    roll back to timeout

--- smallCMS/src/features/pages/dynamicDeepLink.js
+++ smallCMS/src/features/pages/dynamicDeepLink.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function DynamicDeepLink() {
@@ -6,36 +6,23 @@ function DynamicDeepLink() {
 
     
 
-  const [attemptedToOpenApp, setAttemptedToOpenApp] = useState(false);
-
-  useEffect(() => {
+useEffect(() => {
     const deepLinkURL = `com.smallandsimple.ios://${screen}?challengeId=${challengeId}&senderId=${senderId}&isCustom=${isCustom}`;
     
-    const handleVisibilityChange = () => {
-      // Only redirect to App Store if we tried opening the app and it's still in the foreground
-      if (attemptedToOpenApp && !document.hidden) {
-        window.location.href = "https://apps.apple.com/";
-        setAttemptedToOpenApp(false);
-      }
-    };
-
-    // Attempt to open the app
     window.location.href = deepLinkURL;
-    setAttemptedToOpenApp(true);
-    
-    // Add a slight delay before attaching the event listener
-    const timer = setTimeout(() => {
-      document.addEventListener('visibilitychange', handleVisibilityChange);
-    }, 500);
 
-    return () => {
-      clearTimeout(timer);
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-    };
+    const timer = setTimeout(() => {
+      if (document.hidden) {
+        return;
+      }
+      window.location.href = "https://apps.apple.com/";
+    }, 4000);
+
+    return () => clearTimeout(timer);
+
   }, [screen, challengeId, senderId, isCustom]);
 
   return <div>Redirecting...</div>;
-
 }
 
 export default DynamicDeepLink;
