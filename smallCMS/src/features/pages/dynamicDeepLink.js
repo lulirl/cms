6249c550dbae6f8e5bf6a607c5b796ea3bdb8dc7
Ignacio Padilla <ignaciop@nicasource.com>
2023-08-27 14:00:32 -0300

    dynamic link new test

--- smallCMS/src/features/pages/dynamicDeepLink.js
+++ smallCMS/src/features/pages/dynamicDeepLink.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function DynamicDeepLink() {
@@ -6,24 +6,35 @@ function DynamicDeepLink() {
 
     
 
-useEffect(() => {
-  const deepLinkURL = `com.smallandsimple.ios://${screen}?challengeId=${challengeId}&senderId=${senderId}&isCustom=${isCustom}`;
-  window.location.href = deepLinkURL;
+  const [attemptedToOpenApp, setAttemptedToOpenApp] = useState(false);
 
-  const handleVisibilityChange = () => {
-    if (!document.hidden) {
-      window.location.href = "https://apps.apple.com/";
-    }
-  };
+  useEffect(() => {
+    const deepLinkURL = `com.smallandsimple.ios://${screen}?challengeId=${challengeId}&senderId=${senderId}&isCustom=${isCustom}`;
+    
+    const handleVisibilityChange = () => {
+      // Only redirect to App Store if we tried opening the app and it's still in the foreground
+      if (attemptedToOpenApp && !document.hidden) {
+        window.location.href = "https://apps.apple.com/";
+      }
+    };
 
-  document.addEventListener('visibilitychange', handleVisibilityChange);
+    // Attempt to open the app
+    window.location.href = deepLinkURL;
+    setAttemptedToOpenApp(true);
+    
+    // Add a slight delay before attaching the event listener
+    const timer = setTimeout(() => {
+      document.addEventListener('visibilitychange', handleVisibilityChange);
+    }, 500);
 
-  return () => {
-    document.removeEventListener('visibilitychange', handleVisibilityChange);
-  };
-}, [screen, challengeId, senderId, isCustom]);
+    return () => {
+      clearTimeout(timer);
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+  }, [screen, challengeId, senderId, isCustom]);
 
   return <div>Redirecting...</div>;
+
 }
 
 export default DynamicDeepLink;
