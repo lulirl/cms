c460dd30a026331ed45eb4b796ed29ee44837674
Ignacio Padilla <ignaciop@nicasource.com>
2023-10-12 15:01:20 -0300

    deeplink updated to support android

--- smallCMS/src/features/pages/dynamicDeepLink.js
+++ smallCMS/src/features/pages/dynamicDeepLink.js
@@ -4,25 +4,31 @@ import { useParams } from 'react-router-dom';
 function DynamicDeepLink() {
   const { screen, challengeId, senderId, isCustom } = useParams();
 
-    
+  const isAndroid = /Android/i.test(navigator.userAgent);
 
-useEffect(() => {
-    const deepLinkURL = `com.smallandsimple.ios://${screen}?challengeId=${challengeId}&senderId=${senderId}&isCustom=${isCustom}`;
+  useEffect(() => {
+    const deepLinkURL = isAndroid 
+      ? `com.smallandsimple.android://${screen}?challengeId=${challengeId}&senderId=${senderId}&isCustom=${isCustom}`
+      : `com.smallandsimple.ios://${screen}?challengeId=${challengeId}&senderId=${senderId}&isCustom=${isCustom}`;
     
     window.location.href = deepLinkURL;
 
   }, [screen, challengeId, senderId, isCustom]);
 
-useEffect(() => {
+  useEffect(() => {
     const timer = setTimeout(() => {
       if (document.hidden) {
         return;
       }
-      window.location.href = "https://apps.apple.com/us/app/small-and-simple/id1661062603";
+      const storeLink = isAndroid 
+        ? "https://play.google.com/store/apps"
+        : "https://apps.apple.com/us/app/small-and-simple/id1661062603";
+      
+      window.location.href = storeLink;
     }, 3000);
 
     return () => clearTimeout(timer);
-  }, []);
+  }, [isAndroid]);
 
   return <div>Redirecting...</div>;
 }
